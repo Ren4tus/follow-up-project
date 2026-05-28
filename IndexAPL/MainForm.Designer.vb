7fd5e11e1bba462af9f4d4d4161cbf8c3692a5b4
TAEMIN39 <ktmin2003@naver.com>
2026-05-28 15:29:38 +0900

    IndexAPL 디자이너 방식으로 수정

--- IndexAPL/MainForm.Designer.vb
+++ IndexAPL/MainForm.Designer.vb
@@ -22,9 +22,122 @@ Partial Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnRegister = New System.Windows.Forms.Button()
+        Me.cmbGender = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(77, 60)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID (선택):"
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(147, 57)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(152, 19)
+        Me.txtId.TabIndex = 1
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(147, 114)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(152, 19)
+        Me.txtName.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(77, 117)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(25, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "이름:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(77, 176)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(25, 12)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "성별:"
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(147, 236)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(152, 19)
+        Me.txtAge.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(77, 239)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(25, 12)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "나이:"
+        '
+        'btnRegister
+        '
+        Me.btnRegister.Location = New System.Drawing.Point(137, 299)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Size = New System.Drawing.Size(135, 35)
+        Me.btnRegister.TabIndex = 9
+        Me.btnRegister.Text = "등록"
+        Me.btnRegister.UseVisualStyleBackColor = True
+        '
+        'cmbGender
+        '
+        Me.cmbGender.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.cmbGender.FormattingEnabled = True
+        Me.cmbGender.Items.AddRange(New Object() {"M", "F"})
+        Me.cmbGender.Location = New System.Drawing.Point(147, 173)
+        Me.cmbGender.Name = "cmbGender"
+        Me.cmbGender.Size = New System.Drawing.Size(152, 20)
+        Me.cmbGender.TabIndex = 10
+        Me.cmbGender.Text = "M"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 382)
+        Me.Controls.Add(Me.cmbGender)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "MainForm"
         Me.Text = "MainForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnRegister As Button
+    Friend WithEvents cmbGender As ComboBox
 End Class
--- IndexAPL/MainForm.vb
+++ IndexAPL/MainForm.vb
@@ -8,50 +8,128 @@ Imports SharedLibrary
 Public Class MainForm
     Inherits Form
 
-    Private txtId As TextBox
-    Private txtName As TextBox
-    Private cmbGender As ComboBox
-    Private txtAge As TextBox
-    Private btnRegister As Button
+    'Private txtId As TextBox
+    'Private txtName As TextBox
+    'Private cmbGender As ComboBox
+    'Private txtAge As TextBox
+    'Private btnRegister As Button
 
     Public Sub New()
         InitializeComponent()
-        ' Initialize Component manually
+        ' Initialize Component manuallys
         Me.Text = "IndexAPL - 회원 등록"
-        Me.Size = New Size(320, 260)
-        Me.StartPosition = FormStartPosition.CenterScreen
-        Me.FormBorderStyle = FormBorderStyle.FixedDialog
-        Me.MaximizeBox = False
+        'Me.Size = New Size(320, 260)
+        'Me.StartPosition = FormStartPosition.CenterScreen
+        'Me.FormBorderStyle = FormBorderStyle.FixedDialog
+        'Me.MaximizeBox = False
 
-        Dim lblId = New Label() With { .Text = "ID (선택):", .Location = New Point(20, 20), .AutoSize = True }
-        txtId = New TextBox() With { .Location = New Point(100, 20), .Width = 150 }
+        'Dim lblId = New Label() With { .Text = "ID (선택):", .Location = New Point(20, 20), .AutoSize = True }
+        'txtId = New TextBox() With { .Location = New Point(100, 20), .Width = 150 }
 
-        Dim lblName = New Label() With { .Text = "이름:", .Location = New Point(20, 60), .AutoSize = True }
-        txtName = New TextBox() With { .Location = New Point(100, 60), .Width = 150 }
+        'Dim lblName = New Label() With { .Text = "이름:", .Location = New Point(20, 60), .AutoSize = True }
+        'txtName = New TextBox() With { .Location = New Point(100, 60), .Width = 150 }
 
-        Dim lblGender = New Label() With { .Text = "성별:", .Location = New Point(20, 100), .AutoSize = True }
-        cmbGender = New ComboBox() With { .Location = New Point(100, 100), .Width = 150, .DropDownStyle = ComboBoxStyle.DropDownList }
-        cmbGender.Items.AddRange(New String() {"M", "F"})
-        cmbGender.SelectedIndex = 0
+        'Dim lblGender = New Label() With { .Text = "성별:", .Location = New Point(20, 100), .AutoSize = True }
+        'cmbGender = New ComboBox() With { .Location = New Point(100, 100), .Width = 150, .DropDownStyle = ComboBoxStyle.DropDownList }
+        'cmbGender.Items.AddRange(New String() {"M", "F"})
+        'cmbGender.SelectedIndex = 0
 
-        Dim lblAge = New Label() With { .Text = "나이:", .Location = New Point(20, 140), .AutoSize = True }
-        txtAge = New TextBox() With { .Location = New Point(100, 140), .Width = 150 }
+        'Dim lblAge = New Label() With { .Text = "나이:", .Location = New Point(20, 140), .AutoSize = True }
+        'txtAge = New TextBox() With { .Location = New Point(100, 140), .Width = 150 }
 
-        btnRegister = New Button() With { .Text = "등록", .Location = New Point(100, 180), .Width = 150, .Height = 30 }
-        AddHandler btnRegister.Click, AddressOf btnRegister_Click
+        'btnRegister = New Button() With { .Text = "등록", .Location = New Point(100, 180), .Width = 150, .Height = 30 }
+        'AddHandler btnRegister.Click, AddressOf btnRegister_Click
 
-        Me.Controls.Add(lblId)
-        Me.Controls.Add(txtId)
-        Me.Controls.Add(lblName)
-        Me.Controls.Add(txtName)
-        Me.Controls.Add(lblGender)
-        Me.Controls.Add(cmbGender)
-        Me.Controls.Add(lblAge)
-        Me.Controls.Add(txtAge)
-        Me.Controls.Add(btnRegister)
+        'Me.Controls.Add(lblId)
+        'Me.Controls.Add(txtId)
+        'Me.Controls.Add(lblName)
+        'Me.Controls.Add(txtName)
+        'Me.Controls.Add(lblGender)
+        'Me.Controls.Add(cmbGender)
+        'Me.Controls.Add(lblAge)
+        'Me.Controls.Add(txtAge)
+        'Me.Controls.Add(btnRegister)
     End Sub
 
-    Private Sub btnRegister_Click(sender As Object, e As EventArgs)
+    'Private Sub btnRegister_Click(sender As Object, e As EventArgs)
+    '    Dim id As String = txtId.Text.Trim()
+    '    Dim name As String = txtName.Text.Trim()
+    '    Dim gender As String = cmbGender.SelectedItem.ToString()
+    '    Dim age As String = txtAge.Text.Trim()
+
+    '    If String.IsNullOrEmpty(name) Then
+    '        MessageBox.Show("이름을 입력해주세요.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    '        Return
+    '    End If
+
+    '    ' 3. ID 비워둔 경우 자동 생성
+    '    If String.IsNullOrEmpty(id) Then
+    '        id = Guid.NewGuid().ToString("N").Substring(0, 8).ToUpper()
+    '        txtId.Text = id
+    '    End If
+
+    '    ' DB Connection string resolving
+    '    Dim currentDir As String = AppDomain.CurrentDomain.BaseDirectory
+    '    Dim dbPath As String = ""
+    '    While currentDir IsNot Nothing AndAlso currentDir.Length > 3
+    '        If File.Exists(Path.Combine(currentDir, "local.db")) Then
+    '            dbPath = Path.Combine(currentDir, "local.db")
+    '            Exit While
+    '        End If
+    '        currentDir = Directory.GetParent(currentDir)?.FullName
+    '    End While
+
+    '    If String.IsNullOrEmpty(dbPath) Then
+    '        MessageBox.Show("local.db 파일을 찾을 수 없습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    '        Return
+    '    End If
+
+    '    Dim connectionString As String = $"Data Source={dbPath};Version=3;"
+
+    '    Try
+    '        Using conn As New SQLiteConnection(connectionString)
+    '            conn.Open()
+    '            ' 공통 DB 스키마 검사 (SharedUtils)
+    '            SharedUtils.EnsureTableSchema(conn) 
+
+    '            ' 4. ID, Name 중복 검사
+    '            Dim checkQuery As String = "SELECT COUNT(1) FROM UserTable WHERE Id = @Id OR name = @Name"
+    '            Using checkCmd As New SQLiteCommand(checkQuery, conn)
+    '                checkCmd.Parameters.AddWithValue("@Id", id)
+    '                checkCmd.Parameters.AddWithValue("@Name", name)
+
+    '                Dim count As Integer = Convert.ToInt32(checkCmd.ExecuteScalar())
+    '                If count > 0 Then
+    '                    MessageBox.Show("이미 존재하는 ID 또는 이름입니다. (중복 등록 불가)", "오류", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+    '                    Return
+    '                End If
+    '            End Using
+
+    '            ' 5. 등록 (current_process="complete", InputSource="APL")
+    '            Dim insertQuery As String = "INSERT INTO UserTable (Id, name, gender, age, current_process, InputSource) VALUES (@Id, @Name, @Gender, @Age, 'complete', 'APL')"
+    '            Using insertCmd As New SQLiteCommand(insertQuery, conn)
+    '                insertCmd.Parameters.AddWithValue("@Id", id)
+    '                insertCmd.Parameters.AddWithValue("@Name", name)
+    '                insertCmd.Parameters.AddWithValue("@Gender", gender)
+    '                insertCmd.Parameters.AddWithValue("@Age", age)
+
+    '                insertCmd.ExecuteNonQuery()
+    '            End Using
+
+    '            MessageBox.Show($"정상적으로 등록되었습니다." & vbCrLf & $"ID: {id}" & vbCrLf & $"이름: {name}", "등록 완료", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+    '            ' Clear fields after success
+    '            txtId.Clear()
+    '            txtName.Clear()
+    '            txtAge.Clear()
+    '            cmbGender.SelectedIndex = 0
+    '        End Using
+    '    Catch ex As Exception
+    '        MessageBox.Show($"오류 발생: {ex.Message}", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    '    End Try
+    'End Sub
+
+    Private Sub btnRegister_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
         Dim id As String = txtId.Text.Trim()
         Dim name As String = txtName.Text.Trim()
         Dim gender As String = cmbGender.SelectedItem.ToString()
@@ -78,7 +156,7 @@ Public Class MainForm
             End If
             currentDir = Directory.GetParent(currentDir)?.FullName
         End While
-        
+
         If String.IsNullOrEmpty(dbPath) Then
             MessageBox.Show("local.db 파일을 찾을 수 없습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Return
@@ -90,14 +168,14 @@ Public Class MainForm
             Using conn As New SQLiteConnection(connectionString)
                 conn.Open()
                 ' 공통 DB 스키마 검사 (SharedUtils)
-                SharedUtils.EnsureTableSchema(conn) 
+                SharedUtils.EnsureTableSchema(conn)
 
                 ' 4. ID, Name 중복 검사
                 Dim checkQuery As String = "SELECT COUNT(1) FROM UserTable WHERE Id = @Id OR name = @Name"
                 Using checkCmd As New SQLiteCommand(checkQuery, conn)
                     checkCmd.Parameters.AddWithValue("@Id", id)
                     checkCmd.Parameters.AddWithValue("@Name", name)
-                    
+
                     Dim count As Integer = Convert.ToInt32(checkCmd.ExecuteScalar())
                     If count > 0 Then
                         MessageBox.Show("이미 존재하는 ID 또는 이름입니다. (중복 등록 불가)", "오류", MessageBoxButtons.OK, MessageBoxIcon.Warning)
@@ -112,12 +190,12 @@ Public Class MainForm
                     insertCmd.Parameters.AddWithValue("@Name", name)
                     insertCmd.Parameters.AddWithValue("@Gender", gender)
                     insertCmd.Parameters.AddWithValue("@Age", age)
-                    
+
                     insertCmd.ExecuteNonQuery()
                 End Using
 
                 MessageBox.Show($"정상적으로 등록되었습니다." & vbCrLf & $"ID: {id}" & vbCrLf & $"이름: {name}", "등록 완료", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                
+
                 ' Clear fields after success
                 txtId.Clear()
                 txtName.Clear()
